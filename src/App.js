242c8d41f5f92f35766d6e52380cfaa5e4a50d24
Sayush Kamat <50794723+thedarkdivinity@users.noreply.github.com>
2020-05-26 21:59:53 +0530

    React Events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
-import "./styles.css";
+import React, { useState } from "react";
+const App = () => {
+  let [col, setcol] = useState("red");
+  let [text, setText] = useState("CLICK ME");
+  const change = () => {
+    setcol("purple");
+    setText("It hurts ");
+  };
+  const yo = () => {
+setcol("chocolate");
+  };
 
-export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div className="main" style={{ backgroundColor: col }}>
+        <div className="center">
+          <button
+            
+            onMouseLeave={change}
+            onMouseEnter={yo}
+          >
+            {text}
+          </button>
+        </div>
+      </div>
+    </>
   );
-}
+};
+export default App;
